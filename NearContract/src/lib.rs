6d0cf865041c00415154cd26e87a1c897e8276e2
gitakileus <akileus0902@gmail.com>
2022-07-14 21:57:33 -0700

    initial environment

---
+++ NearContract/src/lib.rs
@@ -0,0 +1,27 @@
+impl Contract {
+    /*
+        initialization function (can only be called once).
+        this initializes the contract with default metadata so the
+        user doesn't have to manually type metadata.
+    */
+    #[init]
+    pub fn new_default_meta(owner_id: AccountId) -> Self {
+        /*
+            FILL THIS IN
+        */
+        todo!(); //remove once code is filled in.
+    }
+
+    /*
+        initialization function (can only be called once).
+        this initializes the contract with metadata that was passed in and
+        the owner_id. 
+    */
+    #[init]
+    pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
+        /*
+            FILL THIS IN
+        */
+        todo!(); //remove once code is filled in.
+    }
+}
