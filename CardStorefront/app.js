9466199474ac3e64e7c2f8bea596b9fa4815e1c1
gitakileus <akileus0902@gmail.com>
2022-07-15 10:41:56 -0700

    back end main function

--- CardStorefront/app.js
+++ CardStorefront/app.js
@@ -1,11 +1,65 @@
 //to run : node filename.js
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const nearAPI = require("near-api-js");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+async function main() {
+  // creates keyStore using private key in local storage
+  // *** REQUIRES SignIn using walletConnection.requestSignIn() ***
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+  const { connect, keyStores, WalletConnection } = nearAPI;
+
+  const config = {
+    networkId: "testnet",
+    keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    explorerUrl: "https://explorer.testnet.near.org",
+  };
+
+  // connect to NEAR
+  const near = await connect(config);
+
+  // create wallet connection
+  const wallet = new WalletConnection(near);
+
+  const signIn = () => {
+    wallet.requestSignIn(
+      "example-contract.testnet", // contract requesting access
+      "Example App", // optional
+      "http://YOUR-URL.com/success", // optional
+      "http://YOUR-URL.com/failure" // optional
+    );
+  };
+
+  const signOut = () => {
+    wallet.signOut();
+  };
+
+  if (wallet.isSignedIn()) {
+    doSomething();
+  }
+
+  // returns account Id as string
+  const walletAccountId = wallet.getAccountId();
+
+  // returns account object for transaction signing
+  const walletAccountObj = wallet.account();
+
+  const account = await near.account("akileus0.testnet");
+
+  // gets account balance
+  await account.getAccountBalance();
+  await account.getAccountDetails();
+}
+
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+// app.listen(port, () =>
+//   console.log(`Example app listening at http://localhost:${port}`)
+// );
 
 //visit localhost:3000
 // assuming you have done 1) npm init 2) npm install express
