cfcda643eb9927d244b2c4920a11b7e3a4628121
gitakileus <akileus0902@gmail.com>
2022-07-15 16:59:53 -0700

    clear coding style

--- CardStorefront/app.js
+++ CardStorefront/app.js
@@ -6,52 +6,56 @@ const app = express();
 const port = 3000;
 const { connect, KeyPair, keyStores, WalletConnection } = nearAPI;
 
-async function main() {
-  // creates keyStore using private key in local storage
-  // *** REQUIRES SignIn using walletConnection.requestSignIn() ***
+const fs = require("fs");
+const { async } = require("regenerator-runtime");
+const { response } = require("express");
+const homedir = require("os").homedir();
 
-  // creates a keyStore that searches for keys in .near-credentials
-  // requires credentials stored locally by using a NEAR-CLI command: `near login`
-  // https://docs.near.org/docs/tools/near-cli#near-login
+const ACCOUNT_ID = `${process.env.CONTRACT_NAME}`; // NEAR account tied to the keyPair
+const NETWORK_ID = `${process.env.NODE_ENV}`;
+// path to your custom keyPair location (ex. function access key for example account)
+const KEY_PATH = `/.near-credentials/testnet/${process.env.CONTRACT_NAME}.json`;
 
-  // creates keyStore from a provided file
-  // you will need to pass the location of the .json key pair
+const CREDENTIALS_DIR = ".near-credentials";
+const credentialsPath = require("path").join(homedir, CREDENTIALS_DIR);
+const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
 
-  const fs = require("fs");
-  const homedir = require("os").homedir();
+const config = {
+  networkId: "testnet",
+  keyStore,
+  nodeUrl: "https://rpc.testnet.near.org",
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+  explorerUrl: "https://explorer.testnet.near.org",
+};
 
-  const ACCOUNT_ID = `${process.env.CONTRACT_NAME}`; // NEAR account tied to the keyPair
-  const NETWORK_ID = `${process.env.NODE_ENV}`;
-  // path to your custom keyPair location (ex. function access key for example account)
-  const KEY_PATH = `/.near-credentials/testnet/${process.env.CONTRACT_NAME}.json`;
-
-  const CREDENTIALS_DIR = ".near-credentials";
-  const credentialsPath = require("path").join(homedir, CREDENTIALS_DIR);
-  const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
-
-  const config = {
-    networkId: "testnet",
-    keyStore,
-    nodeUrl: "https://rpc.testnet.near.org",
-    walletUrl: "https://wallet.testnet.near.org",
-    helperUrl: "https://helper.testnet.near.org",
-    explorerUrl: "https://explorer.testnet.near.org",
-  };
-
+async function deploy() {
   // connect to NEAR
   const near = await connect(config);
-
   const account = await near.account(`${process.env.CONTRACT_NAME}`);
 
   // gets account balance
   const balance = await account.getAccountBalance();
-  const detail = await account.getAccountDetails();
+  console.log("Balance:", balance);
 
+  const response = await account.deployContract(
+    fs.readFileSync("../out/main.wasm")
+  );
+  console.log(response);
+}
+
+async function initialize() {
+  // connect to NEAR
+  const near = await connect(config);
+  const account = await near.account(process.env.CONTRACT_NAME);
+
+  // gets account balance
+  const balance = await account.getAccountBalance();
   console.log("Balance:", balance);
 
   const contract = new nearAPI.Contract(
     account, // the account object that is connecting
-    `${process.env.CONTRACT_NAME}`,
+    process.env.CONTRACT_NAME,
     {
       // name of contract you're connecting to
       viewMethods: ["getMessages"], // view methods do not change state but usually return a value
@@ -60,22 +64,19 @@ async function main() {
     }
   );
 
-  await contract.NFTContractMetadata(
+  const response = await contract.initialize(
     {
-      arg_name: "value", // argument name and value - pass empty object if no args required
+      owner_id: process.env.CONTRACT_NAME,
+      metadata: {
+        owner_id: process.env.CONTRACT_NAME,
+      },
+      total_supply: "1000",
+      cost_per_token: "20",
     },
     "300000000000000", // attached GAS (optional)
     "1000000000000000000000000" // attached deposit in yoctoNEAR (optional)
   );
+  console.log(response);
 }
 
-// app.get("/", (req, res) => res.send("Hello World!"));
-
-// app.listen(port, () =>
-//   console.log(`Example app listening at http://localhost:${port}`)
-// );
-
-//visit localhost:3000
-// assuming you have done 1) npm init 2) npm install express
-
-main();
+initialize();
