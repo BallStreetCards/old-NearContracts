5eb7fa11199fa5da3a103f84d2523c5f75aa99e2
akileus <akileus0902@gmail.com>
2022-08-05 12:28:34 -0500

    add get supply of lottery in token contract

--- lottery_contract/src/llib.rs
+++ lottery_contract/src/llib.rs
@@ -4,6 +4,9 @@ use near_sdk::{env, log, near_bindgen, AccountId, Gas, Promise, PromiseError, Pa
 pub mod external;
 pub use crate::external::*;
 
+pub token1: AccountId = "mj-234.tenamint-card.near".parse().unwrap();
+pub token2: AccountId = "mj-234.tenamint-card.near".parse().unwrap();
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
@@ -29,8 +32,38 @@ impl Contract {
     receiver_id: AccountId,
   ) -> Promise {
     let mut attached_deposit = env::attached_deposit();
+    let mut token1_count = 1;
+    let mut token2_count = 1;
+    let mut token1_supply = 
     loop {
-
+      if ()
     }
+  }
+
+  #[private]
+  pub fn query_get_supply(token_id: AccountId) -> Promise {
+    // Create a promise to call token.nft_supply_for_owner function
+    let promise = token_id::ext(token_id.clone())
+      .with_static_gas(Gas(5*TGAS))
+      .nft_supply_for_owner(env::predecessor_account_id());
+    
+    return promise.then(
+      Self::ext(env::current_account_id())
+      .with_static_gas(Gas(5*TGAS))
+      .query_get_supply_callback()
+    )
+  }
+
+  #[private]
+  pub fn query_get_supply_callback(#[callback_result] call_result: Result<U128, PromiseError>) -> String {
+    // Check if the promise succeeded by calling the method outlined in external.rs
+    if call_result.is_err() {
+      log!("There was an error contacting {} contract", token_id);
+      return "".to_string();
+    }
+
+    // return the supply
+    let supply: U128 = call_result.unwrap();
+    supply
   }
 }
